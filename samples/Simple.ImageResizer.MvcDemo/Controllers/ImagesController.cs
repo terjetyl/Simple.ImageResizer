f760729932b117aa9219c9ad5686a49f70120a79
Terje Tyldum <terjetyl@gmail.com>
2013-02-05 19:25:23 +0100

    added outputcache in demo

--- samples/Simple.ImageResizer.MvcDemo/Controllers/ImagesController.cs
+++ samples/Simple.ImageResizer.MvcDemo/Controllers/ImagesController.cs
@@ -6,6 +6,7 @@ namespace Simple.ImageResizer.MvcDemo.Controllers
 {
     public class ImagesController : Controller
     {
+        [OutputCache(VaryByParam = "*", Duration = 60 * 60 * 24 * 365)]
         public ImageResult Index(string filename, int w = 0, int h = 0)
         {
             string filepath = Path.Combine(Server.MapPath("~/images2"), filename);
